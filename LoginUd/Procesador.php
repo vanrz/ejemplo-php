85fda4ca1b24d8fbb433e6ce9e31abdcdd17e500
Estudiantes <Estudiantes@Labing50715.udistrital.edu.co>
2018-04-02 17:43:49 -0500

    parte 1

---
+++ LoginUd/Procesador.php
@@ -0,0 +1,8 @@
+<?php
+mysqli_connect("localhost", "root", "mysql2018", "ud", "3306");
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
---
+++ LoginUd/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="newstyle.css" rel="stylesheet"type="text/css"/>
+        <title>LoginUd</title>
+    </head>
+    <body>
+        <p class="titulo">
+            Login  
+        </p>  
+        <form action="procesador.php">
+            <input type="text" name="usuario"><br>
+            <input type="password" name="clave"><br>
+            <input type="submit" name="boton" value="ingresar"><br>
+            
+        </form>
+    </body>
+</html>
